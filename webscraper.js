0345b63f1808b3bf1c0218953288fd8e9ee328d3
Kananelo12 <kananeloj12@gmail.com>
2025-07-18 19:31:36 +0200

    Did a basic fetch and logged it for testing

--- webscraper.js
+++ webscraper.js
@@ -1,1 +1,8 @@
 // TODO: write this file!
+// import "./fetch-polyfill.js";
+// import fetch from "node-fetch";
+
+const response = await fetch("https://joel-portfolio.web.app/");
+const body = await response.text();
+
+console.log(body);
