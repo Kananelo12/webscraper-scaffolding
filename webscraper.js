2c1afb5d83fbfb6f6d8de27872c8a2c7e02b6ca1
Kananelo12 <kananeloj12@gmail.com>
2025-07-21 14:27:04 +0200

    Basic JSDOM web scraper

--- webscraper.js
+++ webscraper.js
@@ -1,8 +1,61 @@
 // TODO: write this file!
-// import "./fetch-polyfill.js";
 // import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
 
-const response = await fetch("https://joel-portfolio.web.app/");
-const body = await response.text();
+const urlParam = process.argv[2];
 
-console.log(body);
+if (!urlParam) {
+  console.log("Expecting a page url argument");
+  process.exit(1);
+}
+
+/**
+ * This function asynchronously fetches the HTML content of a webpage using the provided
+ * URL.
+ * @param pageUrl - A parameter of type string, representing the webpage to fetch HTML from
+ * @returns Returns a Promise that resolves to the `document` object
+ * of the fetched page's HTML content.
+ */
+const fetchPageHtml = async (pageUrl) => {
+  try {
+    const response = await fetch(pageUrl);
+    const body = await response.text();
+
+    // console.log(body);
+    return new JSDOM(body).window.document;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+const doc = await fetchPageHtml(urlParam);
+
+/**
+ * This function extracts all links starting with "https://" from a document and logs
+ * them to the console.
+ */
+const extractAllLinks = () => {
+  console.log("\n------------------ All Links Scraped ------------------");
+  const links = [...doc.querySelectorAll("a")]
+    .map((a) => a.href)
+    .filter((href) => href.startsWith("https://"));
+
+  console.log(links);
+};
+
+extractAllLinks();
+
+/**
+ * This function scrapes all image URLs from a webpage and logs the absolute URLs.
+ */
+const extractAllImages = () => {
+  const images = [...doc.querySelectorAll("img")].map((img) => img.src);
+
+  console.log("\n------------------All Images Scraped ------------------");
+  for (let image of images) {
+    const absoluteUrl = new URL(image, urlParam).href;
+    console.log("\nImage: ", absoluteUrl);
+  }
+};
+
+extractAllImages();
