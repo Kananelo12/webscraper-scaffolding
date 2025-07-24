fc076bc6c0d55be22bbe6d66a37024696c045581
Kananelo12 <kananeloj12@gmail.com>
2025-07-24 13:11:25 +0200

    Implemented methods for scraping

--- webscraper.js
+++ webscraper.js
@@ -7,6 +7,13 @@ if (!urlParam) {
   process.exit(1);
 }
 
+/**
+ * This function asynchronously fetches the HTML content of a webpage using the provided
+ * URL.
+ * @param pageUrl - A parameter of type string, representing the webpage to fetch HTML from
+ * @returns Returns a Promise that resolves to the `document` object
+ * of the fetched page's HTML content.
+ */
 const fetchPageHtml = async (pageUrl) => {
   try {
     const response = await fetch(pageUrl);
@@ -20,3 +27,35 @@ const fetchPageHtml = async (pageUrl) => {
 };
 
 const doc = await fetchPageHtml(urlParam);
+
+/**
+ * This function extracts all links starting with "https://" from a document and logs
+ * them to the console.
+ */
+const extractLinks = () => {
+  console.log("\n------------------ All Links Scraped ------------------");
+  const links = [...doc.querySelectorAll("a")]
+    .map((a) => a.href)
+    .filter((href) => href.startsWith("https://"));
+
+  const uniqueSortedLinks = [...new Set(links)].sort();
+  console.log(uniqueSortedLinks);
+};
+
+extractLinks();
+
+/**
+ * This function scrapes all image URLs from a webpage and logs the absolute URLs.
+ */
+const extractImages = () => {
+  console.log("\n------------------ All Images Scraped ------------------");
+  const images = [...doc.querySelectorAll("img")].map((img) => img.src);
+
+  const uniqueSortedImages = [...new Set(images)].sort();
+  for (let image of uniqueSortedImages) {
+    const absolutePath = new URL(image, urlParam).href;
+    console.log(absolutePath);
+  }
+};
+
+extractImages();
